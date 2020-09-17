cc47db8313a018d6b537ed98505b23d8f4deb235
isriom <62965230+isriom@users.noreply.github.com>
2020-09-16 22:11:19 -0600

    Main(client) visual display Server visual display

--- Chat/src/Main.java
+++ Chat/src/Main.java
@@ -1,21 +1,52 @@
+import javax.swing.*;
 import java.util.Scanner;
 
 public class Main {
 
-    public static String scan;
-
     public static void main(String[] args) {
         System.out.println("Welcome to the chat system");
         System.out.println("please enter your name:");
-        String scan =Main.name();
+        String scan =Main.write();
         System.out.println(scan);
-        System.out.println("enter a port");
-        int port= Integer.parseInt(Main.name());
-        Server network =new Server(port);
+        System.out.println("enter server port");
+        int port= Integer.parseInt(Main.write());
+        Main.display();
+        try{
+            Server network =new Server(port);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public static String name(){
+    public static String write(){
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
+    }
+    public static void recieve(String data){
+        boolean port_error= data.equals("port_error");
+        boolean port_accept= data.equals("port_accepted");
+        boolean connect= data.equals("connect?");
+        if (port_error){
+            System.out.println("This port see that is not working");
+        }
+        else if(port_accept){
+            System.out.println("This port is working, server is running, connecting");
+
+        }
+        else if (connect){
+            System.out.println("Connect to server?");
+
+        }
+
+    }
+    public static JFrame chat=new JFrame("Client");//creating instance of JFrame
+
+    public static void display(){
+        JTextArea readArea= new JTextArea();
+        readArea.setBounds(10,10, 380,300);
+        Main.chat.setSize(400,400);
+        Main.chat.setLayout(null);
+        Main.chat.setVisible(true);
+        Main.chat.add(readArea);
     }
 }
--- Chat/src/Server.java
+++ Chat/src/Server.java
@@ -1,14 +1,54 @@
 import java.net.*;
 import java.io.*;
+import java.util.List;
+import javax.swing.*;
 
 public class Server {
     public Server(int portnumber ) {
         try {
             ServerSocket port=  new ServerSocket(portnumber);
-            Chat.recieve("port_accepted");
-        } catch (IOException | IllegalArgumentException e) {
-            System.out.println("This port see that is not working or out of range");
-            Chat.recieve("port_error");
+            Main.recieve("port_accepted");
+            Server.Comunication(port);
+        } catch (BindException e){
+            System.out.println("This port connect to a working server");
+            Main.recieve("connect?");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("This port see that is not working ");
+            Main.recieve("port_error");
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            System.out.println("This port see that is out of range");
+            Main.recieve("port_error");
+            e.printStackTrace();
+        }
+    }
+    public static JFrame screen=new JFrame("server");//creating instance of JFrame
+
+    public static void Comunication(ServerSocket socketport) throws IOException {
+        DefaultListModel<Socket> Users = new DefaultListModel<>();
+        boolean WaitUser= true;
+        Socket clientSocket = new Socket();
+        clientSocket.connect(socketport.getLocalSocketAddress());
+        //Users.addElement(clientSocket);
+
+        JList<Socket> userslist= new JList<>(Users);
+        userslist.setBounds(100,100, 250,100);
+        Server.screen.setSize(400,400);
+        Server.screen.setLayout(null);
+        Server.screen.setVisible(true);
+        Server.screen.add(userslist);
+
+        try {
+            while (WaitUser) {
+                Users.addElement(socketport.accept());
+                System.out.println(Users);
+                WaitUser= Users.size()<3;
+
+            }
+            System.out.println("This port see that is out of range");
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
